3caf660a8a552b8c42fec4ebff85e36b45dd4ff2
Sejas <antonio@sejas.es>
2019-08-27 22:18:00 +0100

    refactor(typescript): fix ts errors

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,18 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { ReactNode } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children?: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
