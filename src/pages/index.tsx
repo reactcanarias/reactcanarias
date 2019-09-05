a18a093161a6fd731ada4cbbe4f1d91057935e2d
Sejas <antonio@sejas.es>
2019-09-06 00:45:54 +0100

    feat: change temporarily the default texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,27 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hola Reacter !</h1>
+    <p>Bienvenido a React Canarias.</p>
+    <p>
+      📢 Únete a nuestro :{" "}
+      <a href="https://reactjs.eu/slack" target="blank">
+        Slack
+      </a>
+      .
+    </p>
+
+    <p>
+      💪 Y a nuestro :{" "}
+      <a href="https://reactjs.eu/meetup" target="blank">
+        Meetup
+      </a>
+      .
+    </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Test page 2</Link>
   </Layout>
 )
 
